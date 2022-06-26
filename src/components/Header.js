37302c65294d90ee35b971e235447c251e589668
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-27 00:28:11 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        The<strong> Anime</strong> HQ
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
